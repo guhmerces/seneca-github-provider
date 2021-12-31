527725c335edfbf7244b4e5f63ff4f6379a8b666
Gustavo <guhmerces@hotmail.com>
2021-12-31 13:56:42 -0300

    Add docs for Team commands

--- src/GithubProvider-doc.ts
+++ src/GithubProvider-doc.ts
@@ -20,12 +20,22 @@ const issue_docs = {
   },
 }
 
+const team_docs = {
+  load_team: {
+    desc: 'Load GitHub Team data into an entity.',
+  },
+  save_team: {
+    desc: 'Update GitHub Team data from an entity.',
+  },
+}
+
 const docs = {
   get_info: {
     desc: 'Get information about the provider.',
   },
   ...repo_docs,
   ...issue_docs,
+  ...team_docs,
 }
 
 export default docs
