0004051b7bdae7931e47ba903b823eebeff752ad
Gustavo <guhmerces@hotmail.com>
2022-01-02 21:21:03 -0300

    Add Gist commands to seneca instance

--- src/github-provider.ts
+++ src/github-provider.ts
@@ -44,6 +44,9 @@ function GithubProvider(this: any, _options: any) {
     .message('role:entity,cmd:load,zone:provider,base:github,name:issue', commands.issue.load_issue)
     .message('role:entity,cmd:save,zone:provider,base:github,name:issue', commands.issue.save_issue)
 
+    .message('role:entity,cmd:load,zone:provider,base:github,name:gist', commands.gist.load_gist)
+    .message('role:entity,cmd:save,zone:provider,base:github,name:gist', commands.gist.save_gist)
+
   async function get_info(this: any, _msg: any) {
     return {
       ok: true,
